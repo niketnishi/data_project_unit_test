2881ceaa79219e3695b1c620203e226dcb3bddec
Niket Nishi <niket.nishi@mountblue.io>
2018-09-20 20:13:10 +0530

    completed all 5 parts of data project with sql_queries

--- src/dataProject1.py
+++ src/dataProject1.py
@@ -2,42 +2,49 @@ from matplotlib import pyplot as plt
 from matplotlib import patches as mpatches
 from collections import OrderedDict
 import sys
-import csv
+from src import read_csv_create_table as db
 
-matches_csv = "/home/dell/PycharmProjects/data_project1/data/matches.csv"
-delivery_csv = "/home/dell/PycharmProjects/data_project1/data/deliveries.csv"
+# matches_csv = "/home/dell/PycharmProjects/data_project1/data/matches.csv"
+# delivery_csv = "/home/dell/PycharmProjects/data_project1/data/deliveries.csv"
+
+
+def establish_connection():
+    conn = db.connect_db('live_db', 'root', 'admin@123', 'localhost')
+    return conn
 
 # Plot the number of matches played per year of all the years in IPL.
 
 
-def num_of_match_each_year(match_url):
-    match_handle = open(match_url, 'r')
-    data_dict = {}
-    for match in csv.DictReader(match_handle):
-        if match['season'] not in data_dict:
-            data_dict[match['season']] = 0
-        data_dict[match['season']] += 1
-    match_handle.close()
-    data_dict = OrderedDict(sorted(data_dict.items()))
+def num_of_match_each_year(match_table_name):
+    conn_obj = establish_connection()
+    cursor = conn_obj.cursor()
+    cursor.execute('SELECT season, COUNT(season) FROM {} group by season;'.format(match_table_name))
+    data_dict = dict(cursor)
+    conn_obj.close()
+    for season in sorted(data_dict.keys()):
+        data_dict[season] = int(data_dict[season])
     # print(data_dict)
     return list(data_dict.keys()), list(data_dict.values())
 
 # Plot a stacked bar chart of matches won of all teams over all the years of IPL.
 
 
-def stacked_bar_chart(match_url):
-    match_handle = open(match_url, 'r')
+def stacked_bar_chart(match_table_name):
     data_dict, lst_of_winning_team = {}, []
-    for match in csv.DictReader(match_handle):
-        if match['season'] not in data_dict:
-            data_dict[match['season']] = {}
-        if match['winner'] != "":
-            if match['winner'] not in data_dict[match['season']]:
-                data_dict[match['season']][match['winner']] = 0
-                if match['winner'] not in lst_of_winning_team:
-                    lst_of_winning_team.append(match['winner'])
-            data_dict[match['season']][match['winner']] += 1
-    match_handle.close()
+    conn_obj = establish_connection()
+    cursor = conn_obj.cursor()
+    cursor.execute('SELECT season, winner FROM {};'.format(match_table_name))
+    for match in cursor:
+        season, winner = 0, 1
+        if match[season] not in data_dict:
+            data_dict[match[season]] = {}
+        if match[winner] != "":
+            if match[winner] not in data_dict[match[season]]:
+                data_dict[match[season]][match[winner]] = 0
+                if match[winner] not in lst_of_winning_team:
+                    lst_of_winning_team.append(match[winner])
+            data_dict[match[season]][match[winner]] += 1
+    conn_obj.close()
     data_dict = OrderedDict(sorted(data_dict.items()))
     # print(data_dict)
     return [data_dict, lst_of_winning_team]
@@ -91,87 +98,46 @@ def plot_stacked_bar_graph(lst_of_seasons, matches_per_team):
 # For the year 2016 plot the extra runs conceded per team.
 
 
-def match_2016_extra_run(match_url, delivery_url):
-    match_handle = open(match_url, 'r')
-    delivery_handle = open(delivery_url, 'r')
-    lst_of_match_id, extra_run_per_team = [], {}
-
-    for match in csv.DictReader(match_handle):
-        if match['season'] == '2016':
-            lst_of_match_id.append(match['id'])
-    for delivery in csv.DictReader(delivery_handle):
-        if delivery['match_id'] in lst_of_match_id:
-            if delivery['bowling_team'] not in extra_run_per_team:
-                extra_run_per_team[delivery['bowling_team']] = 0
-            extra_run_per_team[delivery['bowling_team']] += int(delivery['extra_runs'])
-    match_handle.close()
-    delivery_handle.close()
+def match_2016_extra_run(match_table_name, delivery_table_name):
+    conn_obj = establish_connection()
+    cursor = conn_obj.cursor()
+    cursor.execute("SELECT bowling_team, SUM(extra_runs) FROM {0} INNER JOIN {1} ON {0}.id = {1}.match_id WHERE {0}.season = '2016' group by {1}.bowling_team;".format(match_table_name, delivery_table_name))
+    extra_run_per_team = dict(cursor)
+    conn_obj.close()
+    for bowling_team in sorted(extra_run_per_team.keys()):
+        extra_run_per_team[bowling_team] = int(extra_run_per_team[bowling_team])
+    # print(extra_run_per_team)
     return extra_run_per_team
 
 # For the year 2015 plot the top economical bowlers.
 
 
-def match_2015_eco_bowler(match_url, delivery_url):
-    match_handle = open(match_url, 'r')
-    delivery_handle = open(delivery_url, 'r')
-    lst_match_id, economical_bowler, final_economical_bowler, lst_of_bowlers = [], {}, {}, []
-    for match in csv.DictReader(match_handle):      # Reading match_id for bowlers
-        if match['season'] == '2015':
-            lst_match_id.append(match['id'])
-    match_handle.close()
-    # Creating "bowler_dict = {'bowler': {'no_of_balls': val, 'tot_runs_given': val}}" in this format
-    for delivery in csv.DictReader(delivery_handle):
-        if delivery['match_id'] in lst_match_id:
-            if delivery['bowler'] not in economical_bowler:
-                economical_bowler[delivery['bowler']] = {}
-                economical_bowler[delivery['bowler']]['no_of_balls'] = 0
-                economical_bowler[delivery['bowler']]['total_runs_given'] = 0
-            economical_bowler[delivery['bowler']]['no_of_balls'] += 1
-            economical_bowler[delivery['bowler']]['total_runs_given'] += int(delivery['total_runs'])
-    delivery_handle.close()
-    for bowler in economical_bowler:
-        economical_bowler[bowler] = round(economical_bowler[bowler]['total_runs_given'] * 6 /
-                                          economical_bowler[bowler]['no_of_balls'], 1)      # Calculating economic rate
-        # Creating a dictionary {economic_rate: list of bowlers}
-        if economical_bowler[bowler] not in final_economical_bowler:
-            final_economical_bowler[economical_bowler[bowler]] = []
-            final_economical_bowler[economical_bowler[bowler]].append(bowler)
-    final_economical_bowler = OrderedDict(sorted(final_economical_bowler.items()))
-    for bowler_lst in final_economical_bowler.values():
-        lst_of_bowlers += bowler_lst
-    final_economical_bowler = OrderedDict()     # Initializing the variable with empty ordered dictionary
-    for bowler in lst_of_bowlers[:10]:      # Storing the final data to the variable
-        final_economical_bowler[bowler] = economical_bowler[bowler]
-    return final_economical_bowler
+def match_2015_eco_bowler(match_table_name, delivery_table_name):
+    economical_bowler, bowler, total_runs, num_of_balls = {}, 0, 1, 2
+    conn_obj = establish_connection()
+    cursor = conn_obj.cursor()
+    cursor.execute("SELECT bowler, SUM(total_runs), COUNT(over) FROM {0} INNER JOIN {1} ON {0}.id = {1}.match_id WHERE {0}.season = '2015' GROUP BY {1}.bowler;".format(match_table_name, delivery_table_name))
+    for data in cursor:
+        economical_bowler[data[bowler]] = round(float(data[total_runs]) * 6 / float(data[num_of_balls]), 1)
+    conn_obj.close()
+    economical_bowler = OrderedDict(sorted(economical_bowler.items(), key=lambda x: x[1])[:10])
+    # print(economical_bowler)
+    return economical_bowler
 
 # Match summary for players winning man of the match maximum number of times in a given year
 
 
-def match_summary_over_years(match_url, season):
-    match_handle = open(match_url)
-    data_dict, rev_data_dict = {}, {}
-    lst_of_top_players = []
-
-    for match in csv.DictReader(match_handle):  # Counting frequency of player_of_match for each player in a season
-        if match['season'] == season:
-            if match['player_of_match'] not in data_dict:
-                data_dict[match['player_of_match']] = 0
-            data_dict[match['player_of_match']] += 1
+def match_summary_over_years(match_table_name, season):
+    data_dict, player, winning_frequency = {}, 0, 1
+    conn_obj = establish_connection()
+    cursor = conn_obj.cursor()
+    cursor.execute("SELECT player_of_match, COUNT(player_of_match) AS frequency FROM {0} WHERE season = {1} GROUP BY player_of_match ORDER BY frequency DESC;".format(match_table_name, season))
+    for player_data in cursor:
+        data_dict[player_data[player]] = int(player_data[winning_frequency])
+    conn_obj.close()
+    data_dict = OrderedDict(sorted(data_dict.items())[:10])
     # print(data_dict)
-    for player in data_dict:       # Creating a reverse dictionary with values of data_dict as key
-        if data_dict[player] not in rev_data_dict:
-            rev_data_dict[data_dict[player]] = []
-        rev_data_dict[data_dict[player]].append(player)
-
-    rev_data_dict = OrderedDict(sorted(rev_data_dict.items(), reverse=True))
-    # print(rev_data_dict)
-    for val_man_of_match in rev_data_dict:      # Storing top players in a list
-        lst_of_top_players += rev_data_dict[val_man_of_match]
-    rev_data_dict = OrderedDict()       # Initializing with empty dictionary for reusing the variable
-    for player in lst_of_top_players[:10]:      # Storing the first 10 player data
-        rev_data_dict[player] = data_dict[player]
-    # print(rev_data_dict)
-    return rev_data_dict
+    return data_dict
 
 
 def choose_option():
@@ -185,7 +151,7 @@ def choose_option():
         selection_var = eval(input('Enter an integer for the choice given above\n'))
         if isinstance(selection_var, int) and selection_var in [1, 2, 3, 4, 5, 6]:
             if selection_var == 1:
-                lst_of_season, num_of_matches_played = num_of_match_each_year(matches_csv)
+                lst_of_season, num_of_matches_played = num_of_match_each_year('matches')
                 plt.bar(lst_of_season, num_of_matches_played, color=['c', 'hotpink', 'darkslateblue', 'forestgreen',
                                                                      'sienna', 'midnightblue', 'steelblue',
                                                                      'lightslategrey', 'cadetblue', 'lightseagreen'])
@@ -195,11 +161,11 @@ def choose_option():
                 plt.grid(axis='y')
                 plt.show()
             elif selection_var == 2:
-                data_dict, lst_of_winning_team = stacked_bar_chart(matches_csv)
+                data_dict, lst_of_winning_team = stacked_bar_chart('matches')
                 lst_every_team_data = matches_won_by_team(data_dict, sorted(lst_of_winning_team))
                 plot_stacked_bar_graph(data_dict.keys(), lst_every_team_data)
             elif selection_var == 3:
-                extra_run_per_team = match_2016_extra_run(matches_csv, delivery_csv)
+                extra_run_per_team = match_2016_extra_run('matches', 'deliveries')
                 plt.bar(list(extra_run_per_team.keys()), list(extra_run_per_team.values()),
                         color=['c', 'hotpink', 'darkslateblue', 'forestgreen', 'midnightblue', 'steelblue',
                                'lightslategrey', 'lightseagreen'])
@@ -209,7 +175,7 @@ def choose_option():
                 plt.grid(axis='y')
                 plt.show()
             elif selection_var == 4:
-                bowler_eco_rate = match_2015_eco_bowler(matches_csv, delivery_csv)
+                bowler_eco_rate = match_2015_eco_bowler('matches', 'deliveries')
                 plt.bar(bowler_eco_rate.keys(), bowler_eco_rate.values(),
                         color=['c', 'hotpink', 'darkslateblue', 'forestgreen', 'sienna',
                                'midnightblue', 'steelblue', 'lightslategrey', 'cadetblue',
@@ -221,7 +187,7 @@ def choose_option():
                 plt.show()
             elif selection_var == 5:
                 season = eval(input('Enter the year for which you want to track man of the match\n'))
-                player_of_match = match_summary_over_years(matches_csv, str(season))
+                player_of_match = match_summary_over_years('matches', str(season))
                 plt.bar(player_of_match.keys(), player_of_match.values())
                 plt.xlabel('Player Name')
                 plt.ylabel('Max. number of man of the match')
